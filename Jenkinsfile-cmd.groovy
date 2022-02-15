318e9ffcd9004e994513ed4c4c71bbac1d5d1e82
Luis Alberto Guevara Terán <Externo2634@bpd.com.do>
2022-02-14 18:46:59 -0700

    test7

--- Jenkinsfile-cmd.groovy
+++ Jenkinsfile-cmd.groovy
@@ -103,8 +103,9 @@ pipeline {
 
         stage('Unit Test') {
             steps {
+                echo "${SECRET}"
                 labelledShell(label: "Execute Unite Test",
-                        script: 'export JAVA_HOME="C:/Program Files/Java/jdk-15" && java -version && ${MAVEN_PATH}/mvn -DargLine="-test1=${SECRET}" test')
+                        script: 'export JAVA_HOME="C:/Program Files/Java/jdk-15" && java -version && ${MAVEN_PATH}/mvn test')
             }
         }
 
